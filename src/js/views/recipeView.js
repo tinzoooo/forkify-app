415da9ce6d3ebf8625e480546cb1de3f0b0dd0c4
tinzoooo <tinzoo.official@gmail.com>
2023-06-09 21:32:23 +0200

    Fixing bugs

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 import View from './View.js';
+const Fraction = require('fractional').Fraction;
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
